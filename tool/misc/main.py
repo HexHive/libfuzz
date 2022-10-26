afa846778506a93569b0f7185957c321615c6e32
Zurab Tsinadze <zurab.tsinadze@epfl.ch>
2022-10-26 13:46:14 +0000

    parsing for field names work
    TODO: refactor code, split it into class and main

--- tool/misc/main.py
+++ tool/misc/main.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3.9
 
-import argparse, tempfile, json, copy
-from symbol import return_stmt
-import sys, os, re
+import argparse, tempfile, json
+import os, re
 import clang.cindex
 
 
-function_declarations = [] # List of AST node objects that are fucntion declarations
-type_incomplete = set()    # List of incomplete types
-apis_definition = []       # List of APIs with original argument types and extra info (e.g., const)
+field_names_and_types_map = {}
 
 
 def get_base_type(type_str):
@@ -19,204 +16,77 @@ def get_base_type(type_str):
         type_str = re.sub('\[\d*\]', '*', type_str)
     return type_str
 
-def get_info(type_str):
 
-    info = {}
-
-    if type_str.startswith("const "):
-        info["const"] = True
-        info["type_clang"] = type_str[len("const "):]
-    else:
-        info["const"] = False
-        info["type_clang"] = type_str
-
-    # ducking ugly
-    info["type_clang"] = info["type_clang"].replace(" ", "").replace("unsigned", "unsigned ").replace("*", "")
-    
-    # stuffs like char[100] into char*
-    if "[" in info["type_clang"]:
-        info["type_clang"] = re.sub('\[\d*\]', '*', info["type_clang"])
-
-    return info
-
-# generate an API structur from the AST node
-def get_api(node):
-    # {"function_name": "NotConfigured", "is_vararg": false,
-    #           "return_info": {"name": "return", "flag": "val", "size": 32, "type": "i32"},
-    #           "arguments_info": [{"name": "tif", "flag": "ref", "size": 64, "type": "%struct.tiff*"}, {"name": "scheme", "flag": "val", "size": 32, "type": "i32"}]}
-
-    api_obj = {}
-
-    function_name = node.displayname[:node.displayname.index("(")]
-    api_obj["function_name"] = function_name
-
-    nt = node.type
-
-    rt = nt.get_result()
-    rt_str = rt.spelling
-    api_obj["return_info"] = get_info(rt_str)
-
-    arguments_info = []
-    for a in nt.argument_types():
-        a_str = a.spelling
-        info = get_info(a_str)
-        arguments_info.append(copy.deepcopy(info))
-    api_obj["arguments_info"] = arguments_info
-
-    return api_obj
-
-def find_return_and_param_types(node, function_name, output_data):
+def find_return_and_param_types(node, function_name):
     if node.displayname.startswith(f"{function_name}(") and node.type.kind == clang.cindex.TypeKind.FUNCTIONPROTO:
-        print(node.displayname)
-        print(node.result_type.kind)
-
         rt = node.type.get_result()
-        print(get_base_type(rt.spelling))
-        output_data["return"] = {"type": get_base_type(rt.spelling)}
-        return True
-        # TODO: handle params
-        # for a in node.type.argument_types():
-        #     a_str = a.spelling
-        #     print(a_str)
-        # for idx, param in enumerate(children[1:]):
-        #     # print(param.displayname)
-        #     if list(param.get_children()):
-        #         param_type = list(param.get_children())[0]
-        #         output_data["params"][f"param_{idx}"] = param_type.displayname
-            # print(child.displayname)
-            # for ch in child.get_children():
-                # print(ch.displayname)
-
-            # print(child.type.kind)
+        result = {"return": get_base_type(rt.spelling)}
+        for idx, a in enumerate(node.type.argument_types()):
+            a_str = get_base_type(a.spelling)
+            result[f"param_{idx}"] = a_str        
+        return True, result
 
     for child in node.get_children():
-        if find_return_and_param_types(child, function_name, output_data):
-            return
-
-    # Recurse for children of this node
-    # if "TIFF" == node.displayname and node.type.kind == clang.cindex.TypeKind.TYPEDEF:
-    #     # print(node.displayname)
-    #     # print(node.kind)
-    #     # print(node.access_specifier)
-    #     # print(node.type.kind)
-    #     # print(node.result_type.kind)
-    #     # for arg in node.get_arguments():
-    #         # print(arg)
-    #     for child in node.get_children():
-    #         print("hello world")
-    #         print(child.displayname)
-    #         print(child.type.kind)
-    #     # print(node.get_children()[0].displayname)
-    #     return
-    
-    # if "tiff" == node.displayname and node.type.kind == clang.cindex.TypeKind.RECORD:
-    #     # print(node.displayname)
-    #     # print(node.kind)
-    #     # print(node.access_specifier)
-    #     # print(node.type.kind)
-    #     # print(node.result_type.kind)
-    #     # for arg in node.get_arguments():
-    #         # print(arg)
-    #     children = []
-    #     for child in node.get_children():
-    #         children.append(child.displayname)
-    #         # print(child.displayname)
-    #         # print(child.kind)
-    #     # print(node.get_children()[0].displayname)
-    #     if children:
-    #         print(len(children))
-    #         print(children[9])
-    #     # print(children)
-    #     return
-
-    
-
-    # if node.type.kind == clang.cindex.TypeKind.FUNCTIONPROTO and include_folder in str(node.location.file):
-    #     function_declarations.append(node)
-    #     apis_definition.append(get_api(node))
-
-    # Print out information about the node
-    # if "TIFF" == node.displayname:
-    #     print(node.displayname)
-    # print('Found %s [line=%s, col=%s] %s' % (node.displayname, node.location.line, node.location.column, node.type))
-        # from IPython import embed; embed(); exit()
-
-    # if node.type.kind == clang.cindex.TypeKind.TYPEDEF and node.type.get_size() == -2:
-    #     for child in node.get_children():
-    #         if child.kind == clang.cindex.CursorKind.TYPE_REF:
-    #             type_incomplete.add("%" + child.displayname.replace(" ","."))
-
-
-def translate_fields(node, fields, type_name):
-    fields = list(filter(lambda x: x != -1, fields))
-    if not fields:
-        return False, []
-
-    if len(fields) != 1:
-        return False, []
-    
-
-    if type_name == node.displayname and node.type.kind == clang.cindex.TypeKind.TYPEDEF:
-        for child in node.get_children():
-            succ, res = translate_fields(child, fields, type_name)
-            if succ:
-                return True, res
-        # print(node.result_type.kind)
-        # for arg in node.get_arguments():
-            # print(arg)
-        # for child in node.get_children():
-        #     succ, res = translate_fields(child, fields, type_name)
-        #     if succ:
-        #         return res
-        # print(node.get_children()[0].displayname)
-        return True, []
-
-    if type_name == node.displayname and node.type.kind == clang.cindex.TypeKind.RECORD:
-        print(node.displayname)
-        print(node.kind)
-        print(node.access_specifier)
-        print(node.type.kind)
-        # print(node.result_type.kind)
-        # for arg in node.get_arguments():
-            # print(arg)
-        # children = []
-        # for child in node.get_children():
-        #     children.append(child.displayname)
-        #     # print(child.displayname)
-        #     # print(child.kind)
-        # # print(node.get_children()[0].displayname)
-        # if children:
-        #     print(len(children))
-        #     print(children[9])
-        # print(children)
-        return True, []
-
-    for child in node.get_children():
-        succ, res = translate_fields(child, fields, type_name)
+        succ, res = find_return_and_param_types(child, function_name)
         if succ:
             return True, res
 
-    return False, []
-    # if "tiff" == node.displayname and node.type.kind == clang.cindex.TypeKind.RECORD:
-    #     print(node.displayname)
-    #     print(node.kind)
-    #     print(node.actranslate_fieldsss_specifier)
-    #     print(node.type.kind)
-    #     print(node.result_type.kind)
-    #     # for arg in node.get_arguments():
-    #         # print(arg)
-    #     # children = []
-    #     # for child in node.get_children():
-    #     #     children.append(child.displayname)
-    #     #     # print(child.displayname)
-    #     #     # print(child.kind)
-    #     # # print(node.get_children()[0].displayname)
-    #     # if children:
-    #     #     print(len(children))
-    #     #     print(children[9])
-    #     # # print(children)
-    #     return
+    return False, {}
+    
 
+def find_field_names_and_types(node, type_name):
+    if type_name in node.displayname and node.type.kind == clang.cindex.TypeKind.RECORD:
+        field_names_and_types = []
+        for child in node.get_children():
+            children = list(child.get_children())
+            if len(children) == 0:
+                type = 'basic_type'
+            elif len(children) == 1:
+                type = children[0].displayname
+            else:
+                continue
+
+            field_names_and_types.append((type if type else "other_type", child.displayname))
+
+        if field_names_and_types:
+            return True, field_names_and_types
+    
+    elif type_name in node.displayname and node.type.kind == clang.cindex.TypeKind.TYPEDEF:
+        field_names_and_types = []
+        for child in node.get_children():
+            if child.kind == clang.cindex.CursorKind.STRUCT_DECL:
+                for child in child.get_children():
+                    children = list(child.get_children())
+                    if len(children) == 0:
+                        type = 'basic_type'
+                    elif len(children) == 1:
+                        type = children[0].displayname
+                    else:
+                        continue
+
+                    field_names_and_types.append((type if type else "other_type", child.displayname))
+
+        if field_names_and_types:
+            return True, field_names_and_types
+
+    for child in node.get_children():
+        succ, res = find_field_names_and_types(child, type_name)
+        if succ:
+            return True, res
+
+    return False, ""
+
+def find_base_type(node, type):
+    if type == node.displayname and node.type.kind == clang.cindex.TypeKind.TYPEDEF:
+        for child in node.get_children():
+            if child.displayname:
+                return True, child.displayname.replace("struct ", "")
+    for child in node.get_children():
+        succ, res = find_base_type(child, type)
+        if succ:
+            return succ, res
+
+    return False, ''
 
 def get_stub_file(include_folder):
 
@@ -237,6 +107,57 @@ def get_stub_file(include_folder):
 
     return stub_file
 
+def add_field_names_to_function_data(root, function_data):
+    function_name = function_data["functionName"]
+
+    succ, function_types = find_return_and_param_types(root, function_name)
+    if not succ:
+        print(f"Could not find return and param types for function '{function_name}'")
+        return False, {}
+    
+    for name, type in function_types.items():
+        print(name)
+        succ, base_type = find_base_type(root, type)
+        if not succ:
+            base_type = type
+        
+        succ, field_names_and_types = find_field_names_and_types(root, base_type)
+        if not succ:
+            print(f"Could not find field names and types for '{base_type}'")
+            for access in function_data[name]:
+                access["field_name"] = "N/A"
+            continue
+
+        field_names_and_types_map[base_type] = field_names_and_types
+    
+
+        
+        
+        for access in function_data[name]:
+            print(access)
+            access_types_list = [base_type]
+            access_param_names_list = []
+            fields = [field for field in access["fields"] if field != -1]
+            for field_idx in fields:
+                prev_field_type = access_types_list[-1]
+                if prev_field_type in ["other_type", "basic_type"]:
+                    continue
+                if prev_field_type in field_names_and_types_map:
+                    access_types_list.append(field_names_and_types_map[prev_field_type][field_idx][0])
+                    access_param_names_list.append(field_names_and_types_map[prev_field_type][field_idx][1])
+                else:
+                    succ, field_names_and_types = find_field_names_and_types(root, prev_field_type)
+                    field_names_and_types_map[prev_field_type] = field_names_and_types
+                    access_types_list.append(field_names_and_types[field_idx][0])
+                    access_param_names_list.append(field_names_and_types[field_idx][1])
+            
+            if access_param_names_list:
+                access["field_name"] = access_param_names_list[-1]
+            else:
+                access["field_name"] = "N/A"
+    return function_data
+
+
 def _main():
 
     parser = argparse.ArgumentParser(description='Extract list of exprted function from header files.')
@@ -255,12 +176,6 @@ def _main():
 
     tmp_file = get_stub_file(include_folder)
 
-    function_name = extractor_data[0]["functionName"]
-    output_data = {}
-    output_data["functionName"] = function_name
-    output_data["return"] = {}
-    output_data["params"] = {}
-
 
     clang.cindex.Config.set_library_file("/usr/local/lib/python3.9/dist-packages/clang/native/libclang.so")
     index = clang.cindex.Index.create()
@@ -270,24 +185,15 @@ def _main():
 
     root = tu.cursor        # Get the root of the AST
 
+    output = []
+    for function_data in extractor_data:
+        data = add_field_names_to_function_data(root, function_data)
+        output.append(data)
+    
+    with open(output_file, "w") as out:
+        json.dump(data, out, indent=4)
 
-    find_return_and_param_types(root, function_name, output_data)
-    print(output_data)
-
-    output_data["return"]["access"] = []
-    return_access = extractor_data[0]["return"]
-    for ra in return_access:
-        access_type = ra["access"]
-        fields = ra["fields"]
-        translate_fields(root, fields, output_data["return"]["type"])
-        output_data["return"]["access"].append(ra)
-    # print(return_access)
-
-
-    # with open(output_file, 'w') as out_f:
-    #     for f in function_declarations:
-    #         out_f.write(f"{f.displayname}\n")
-
+    
 
 if __name__ == "__main__":
     _main()
