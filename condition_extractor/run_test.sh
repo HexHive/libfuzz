ef0c638e1143ffe612738da99533b7554f250a62
Flavio Toffalini <flavio87@gmail.com>
2022-10-28 13:12:58 +0000

    some test with libtiff drivers

--- condition_extractor/run_test.sh
+++ condition_extractor/run_test.sh
@@ -3,8 +3,9 @@
 # ./bin/extractor /workspaces/libfuzz/analysis/libtiff/work/lib/libtiff.a.bc -function TIFFClientOpen -output my_output_O0.json -t json -v v1
 # ./bin/extractor /workspaces/libfuzz/analysis/libtiff/work/lib/libtiff.a.bc -function TIFFCleanup 
 
-FUNCTION_NAME="TIFFReadDirectory"
+# FUNCTION_NAME="TIFFReadDirectory"
 
-./bin/extractor /workspaces/libfuzz/analysis/libtiff/work/lib/libtiff.a.bc -function ${FUNCTION_NAME} -output  ${FUNCTION_NAME}_O0.json -t json -v v1
-
-./bin/extractor /workspaces/libfuzz/analysis/libtiff_O3/work/lib/libtiff.a.bc -function ${FUNCTION_NAME} -output  ${FUNCTION_NAME}_O3.json -t json -v v1
+for FUNCTION_NAME in TIFFGetField TIFFTileSize64 TIFFClose _TIFFmalloc TIFFReadRGBAImage _TIFFfree 
+do
+    ./bin/extractor /workspaces/libfuzz/analysis/libtiff/work/lib/libtiff.a.bc -function ${FUNCTION_NAME} -output  ${FUNCTION_NAME}.json -t json -v v1
+done
