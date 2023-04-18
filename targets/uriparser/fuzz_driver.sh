da666ef9e4fc60ad0287a618066dffccbc030621
Flavio Toffalini <flavio87@gmail.com>
2023-04-18 16:42:11 +0000

    missing file

--- targets/uriparser/fuzz_driver.sh
+++ targets/uriparser/fuzz_driver.sh
@@ -30,25 +30,24 @@ echo "Compiling: ${DRIVER_FOLDER}/${DRIVER}.cc"
 for d in `ls ${DRIVER_FOLDER}/${DRIVER}.cc`
 do
     echo "Driver: $d"
-    $CXX -g -std=c++11  -fsanitize=fuzzer,address -I/${TARGET}/work/include \
-        $d ${TARGET}/work/lib/libtiff.a ${TARGET}/work/lib/libtiffxx.a \
-        -lz -ljpeg -Wl,-Bstatic -llzma -Wl,-Bdynamic -lstdc++ -o "${d%%.*}"
+    $CXX -g -std=c++11 -fsanitize=fuzzer,address I/${TARGET}/work/include \
+    ${TARGET}/work/lib/liburiparser.a -o "${d%%.*}"
 done
 
-# for d in ` find ${DRIVER_FOLDER} -type f -executable -name "{$DRIVER}"`
-# do
-#     DRIVER_NAME=$(basename $d)
-#     echo "Fuzzing ${TIMEOUT}: ${DRIVER_NAME}"
-#     DRIVER_CORPUS=${LIBFUZZ}/workdir/${TARGET_NAME}/corpus/${DRIVER_NAME}
-#     CRASHES_DIR=${LIBFUZZ}/workdir/${TARGET_NAME}/crashes/${DRIVER_NAME}
-#     mkdir -p ${CRASHES_DIR}
+for d in ` find ${DRIVER_FOLDER} -type f -executable -name "{$DRIVER}"`
+do
+    DRIVER_NAME=$(basename $d)
+    echo "Fuzzing ${TIMEOUT}: ${DRIVER_NAME}"
+    DRIVER_CORPUS=${LIBFUZZ}/workdir/${TARGET_NAME}/corpus/${DRIVER_NAME}
+    CRASHES_DIR=${LIBFUZZ}/workdir/${TARGET_NAME}/crashes/${DRIVER_NAME}
+    mkdir -p ${CRASHES_DIR}
     
-#     # THIS IS WITH STANDARD MODE -- STOP AT THE FIRST CRASHE
-#     # timeout $TIMEOUT $d ${DRIVER_CORPUS} \
-#     #     -artifact_prefix=${CRASHES_DIR}/ || echo "Done: $d"
+    # THIS IS WITH STANDARD MODE -- STOP AT THE FIRST CRASHE
+    # timeout $TIMEOUT $d ${DRIVER_CORPUS} \
+    #     -artifact_prefix=${CRASHES_DIR}/ || echo "Done: $d"
 
-#     # THIS IS WITH FORK-MODE -- KEEP GOING TILL SOMEONE KILLS IT
-#     (sleep $TIMEOUT && pkill ${DRIVER_NAME}) &
-#     $d ${DRIVER_CORPUS} -artifact_prefix=${CRASHES_DIR}/ -ignore_crashes=1 \
-#         -ignore_timeouts=1 -ignore_ooms=1 -fork=1 || echo "Done: $d"
-# done
+    # THIS IS WITH FORK-MODE -- KEEP GOING TILL SOMEONE KILLS IT
+    (sleep $TIMEOUT && pkill ${DRIVER_NAME}) &
+    $d ${DRIVER_CORPUS} -artifact_prefix=${CRASHES_DIR}/ -ignore_crashes=1 \
+        -ignore_timeouts=1 -ignore_ooms=1 -fork=1 || echo "Done: $d"
+done
