778808e66dd4b8910a4b461fe8088d4525657e7d
Nicolas Badoux <n.badoux@hotmail.com>
2023-05-02 11:04:25 +0200

    minijail analysis good

---
+++ targets/minijail/build_library.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+##
+# Pre-requirements:
+# - env TARGET: path to target work dir
+# - env OUT: path to directory where artifacts are stored
+# - env CC, CXX, FLAGS, LIBS, etc...
+##
+
+# export TARGET=/tmp/libtiff
+
+if [ ! -d "$TARGET/repo" ]; then
+    echo "fetch.sh must be executed first."
+    exit 1
+fi
+
+export CC=$LLVM_DIR/bin/clang
+export CXX=$LLVM_DIR/bin/clang++
+
+WORK="$TARGET/work"
+rm -rf "$WORK"
+mkdir -p "$WORK"
+mkdir -p "$WORK/lib" "$WORK/include"
+
+echo "make 1"
+cd "$TARGET/repo"
+        
+echo "make clean"
+make -j$(nproc) clean
+echo "make"
+make -j$(nproc) OUT="$WORK/lib" CC_STATIC_LIBRARY\(libminijail.pie.a\)
+cp $WORK/*.h $WORK/include
+
+echo "[INFO] Library installed in: $WORK"
---
+++ targets/minijail/compile_driver.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+##
+# Pre-requirements:
+# - env TARGET: path to target work dir
+# - env OUT: path to directory where artifacts are stored
+# - env CC, CXX, FLAGS, LIBS, etc...
+##
+
+# if [ ! -d "$TARGET/repo" ]; then
+#     echo "fetch.sh must be executed first."
+#     exit 1
+# fi
+
+WORK="$TARGET/work"
+
+cd "$TARGET/repo"
+
+CXX=$LLVM_DIR/bin/clang++
+CC=$LLVM_DIR/bin/clang
+
+echo "Compiling: ${DRIVER_FOLDER}/${DRIVER}.cc"
+
+# [TAG] FIRST LOOP FOR COMPILATION!!! 
+for d in `ls ${DRIVER_FOLDER}/${DRIVER}.cc`
+do
+    echo "Driver: $d"
+    # [TAG] THIS STEP MUST BE ADAPTED FOR EACH LIBRARY
+    $CXX -g -std=c++11  -fsanitize=fuzzer,address -I/${TARGET}/work/include \
+        $d ${TARGET}/work/lib/libminijail.pie.a \
+        -lz -ljpeg -Wl,-Bstatic -llzma -Wl,-Bdynamic -lstdc++ -o "${d%%.*}"
+done
