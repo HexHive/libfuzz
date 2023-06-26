599a547897d179ef06e5f0e408f275bde23964ab
tregua <flavio87@gmail.com>
2023-06-26 15:14:08 +0000

    analysys 0.1

--- targets/openssl/analysis.sh
+++ targets/openssl/analysis.sh
@@ -31,7 +31,7 @@ cd "$TARGET/repo"
 # the config script supports env var LDLIBS instead of LIBS
 export LDLIBS="$LIBS"
 
-./config --debug $CFLAGS -fno-sanitize=alignment $CONFIGURE_FLAGS --prefix="$WORK"
+./config --debug $CFLAGS -fno-sanitize=alignment --prefix="$WORK"
 
 # configure compiles some shits for testing, better remove it
 rm -f $LIBFUZZ_LOG_PATH/apis_clang.json
@@ -39,6 +39,7 @@ rm -f $LIBFUZZ_LOG_PATH/apis_clang.json
 touch $LIBFUZZ_LOG_PATH/exported_functions.txt
 touch $LIBFUZZ_LOG_PATH/incomplete_types.txt
 touch $LIBFUZZ_LOG_PATH/apis_clang.json
+touch $LIBFUZZ_LOG_PATH/apis_llvm.json
 touch $LIBFUZZ_LOG_PATH/coerce.log
 
 # CXXFLAGS="-fPIC -DOPENSSL_PIC"
@@ -52,13 +53,17 @@ extract-bc -b $WORK/lib/libssl.a
 
 # this extracts the exported functions in a file, to be used later for grammar generations
 $LIBFUZZ/tool/misc/extract_included_functions.py -i "$WORK/include/openssl" \
-                                                 -e "$LIBFUZZ_LOG_PATH/exported_functions.txt" \
-                                                 -t "$LIBFUZZ_LOG_PATH/incomplete_types.txt" \
-                                                 -a "$LIBFUZZ_LOG_PATH/apis_clang.json"
+    -p "$LIBFUZZ/targets/${TARGET_NAME}/public_headers.txt" \
+    -e "$LIBFUZZ_LOG_PATH/exported_functions.txt" \
+    -t "$LIBFUZZ_LOG_PATH/incomplete_types.txt" \
+    -a "$LIBFUZZ_LOG_PATH/apis_clang.json" \
+    -n "$LIBFUZZ_LOG_PATH/enum_types.txt"
 
 # TODO: this should get the list of apis, not a single functions
-# $LIBFUZZ/condition_extractor/bin/extractor $WORK/lib/libssl.a.bc -function ssl3_get_cipher_by_id -output $LIBFUZZ_LOG_PATH/conditions.json -v v0 -t json
-# echo "$LIBFUZZ_LOG_PATH/conditions.json"
-# $LIBFUZZ/condition_extractor/bin/extractor $WORK/lib/libssl.a.bc -interface $LIBFUZZ_LOG_PATH/apis_clang.json -output $LIBFUZZ_LOG_PATH/conditions.json -v v0 -t json -do_indirect_jumps -data_layout $LIBFUZZ_LOG_PATH/data_layout.txt
-
-# echo "$LIBFUZZ_LOG_PATH/conditions.json"
+$TOOLS_DIR/condition_extractor/bin/extractor \
+    $WORK/lib/libssl.a.bc \
+    -interface "$LIBFUZZ_LOG_PATH/apis_clang.json" \
+    -output "$LIBFUZZ_LOG_PATH/conditions.json" \
+    -minimize_api "$LIBFUZZ_LOG_PATH/apis_minimized.txt" \
+    -v v0 -t json -do_indirect_jumps \
+    -data_layout "$LIBFUZZ_LOG_PATH/data_layout.txt"
