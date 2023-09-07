127a166a8ea72c06d137a6658b1b8eab104f306f
Flavio Toffalini <flavio87@gmail.com>
2023-09-07 15:44:49 +0200

    update preliminary analysis configuration

--- fuzzing_campaigns/campaign_configuration.sh
+++ fuzzing_campaigns/campaign_configuration.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 export PROJECTS=( "cpu_features" "minijail" "pthreadpool" "libhtp" "libvpx" "libtiff" "libaom" )
-export NUM_OF_DRIVERS=( 5 10 20 )
-export NUM_OF_APIs=( 3 6 12 )
+export NUM_OF_DRIVERS=( 2 4 8 16 32 )
+export NUM_OF_APIs=( 20 )
 export NUM_OF_SEEDS=20
 # export POLICY="constraint_based"
 export POLICY="constraint_based_weigth"
-export TIMEOUT=5m
+export TIMEOUT=10m
 export ITERATIONS=1
 export MAX_CPUs=$(($(nproc) - 1))
