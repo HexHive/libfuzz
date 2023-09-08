f2f9dd3a465aca8c50823f4673ad25d2e321ab7a
Zurab Tsinadze <zurab.tsinadze@epfl.ch>
2023-09-08 09:15:37 +0000

    coverage per driver, configuration for stable campaings

--- fuzzing_campaigns/run_coverage.sh
+++ fuzzing_campaigns/run_coverage.sh
@@ -18,21 +18,35 @@ for project in "${PROJECTS[@]}"; do
     set +x
 done
 
-for project in "${PROJECTS[@]}"; do
-    for ndrivers in "${NUM_OF_DRIVERS[@]}"; do
-        for napis in "${NUM_OF_APIs[@]}"; do
-            for i in $( eval echo {1..$ITERATIONS} ); do
-                    PROJECT_FOLDER="/workspaces/libfuzz/fuzzing_campaigns/workdir_${ndrivers}_${napis}/${project}"
-                    DRIVER_FOLDER="${PROJECT_FOLDER}/drivers"
-                    CORPUS_FOLDER="${PROJECT_FOLDER}/results/iter_${i}/corpus_new"
-                    COVERAGE_FOLDER="/workspaces/libfuzz/fuzzing_campaigns/workdir_${ndrivers}_${napis}/${project}/coverage_data/iter_${i}"
-                    docker run --env DRIVER_FOLDER=${DRIVER_FOLDER} --env PROJECT_COVERAGE=${COVERAGE_FOLDER} --env TARGET=${project} --env CORPUS_FOLDER=${CORPUS_FOLDER} \
-                        -v $(pwd)/..:/workspaces/libfuzz "${IMG_NAME}-${project}"
 
-            done
+for ndrivers in "${NUM_OF_DRIVERS[@]}"; do
+    for napis in "${NUM_OF_APIs[@]}"; do
+        for project in "${PROJECTS[@]}"; do
+            PROJECT_FOLDER="/workspaces/libfuzz/fuzzing_campaigns/workdir_${ndrivers}_${napis}/${project}"
+            # for i in $( eval echo {1..$ITERATIONS} ); do
+            #     DRIVER_FOLDER="${PROJECT_FOLDER}/drivers"
+            #     CORPUS_FOLDER="${PROJECT_FOLDER}/results/iter_${i}/corpus_new"
+            #     COVERAGE_FOLDER="/workspaces/libfuzz/fuzzing_campaigns/workdir_${ndrivers}_${napis}/${project}/coverage_data/iter_${i}"
+            #     docker run --env DRIVER_FOLDER=${DRIVER_FOLDER} --env PROJECT_COVERAGE=${COVERAGE_FOLDER} --env TARGET=${project} --env CORPUS_FOLDER=${CORPUS_FOLDER} \
+            #         -v $(pwd)/..:/workspaces/libfuzz "${IMG_NAME}-${project}"
+            # done
+            # docker run --env TOTAL_DRIVER_COVERAGE="YES" --env PROJECT_FOLDER=${PROJECT_FOLDER} -v $(pwd)/..:/workspaces/libfuzz "${IMG_NAME}-${project}"
+            docker run --env TOTAL_LIBRARY_COVERAGE_FOR_CONFIGURATION="YES" --env PROJECT_FOLDER=${PROJECT_FOLDER} -v $(pwd)/..:/workspaces/libfuzz "${IMG_NAME}-${project}"
         done
     done
 done
+
+
+rm total_library_coverage.csv || true
+touch total_library_coverage.csv
+
+for project in "${PROJECTS[@]}"; do
+    docker run --env TOTAL_LIBRARY_COVERAGE="YES" -v $(pwd)/..:/workspaces/libfuzz "${IMG_NAME}-${project}"
+    LIBRARY_COVERAGE_REPORT="./total_library_coverage/${project}/report"
+    total_coverage=$(tail -n 1 $LIBRARY_COVERAGE_REPORT | awk '{print $4}')
+    echo "${project},${total_coverage}" >> total_library_coverage.csv
+done
+
 
 rm ../crash-*
 rm ../oom-*
