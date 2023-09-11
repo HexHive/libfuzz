33d12a3ba75a5c3f3e2f188b361f80b031d720a0
Nicolas Badoux <nicolas.badoux@epfl.ch>
2023-09-11 09:01:43 +0000

    c-ares analysis working

---
+++ targets/c-ares/compile_driver.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+##
+# Pre-requirements:
+# - env TARGET: path to target work dir
+# - env OUT: path to directory where artifacts are stored
+# - env CC, CXX, FLAGS, LIBS, etc...
+##
+
+# if [ ! -d "$TARGET/repo" ]; then
+#     echo "fetch.sh must be executed first."
+#     exit 1
+# fi
+
+cd "$TARGET/repo"
+
+CXX=$LLVM_DIR/bin/clang++
+
+echo "Compiling: ${DRIVER_FOLDER}/${DRIVER}.cc"
+mkdir -p "${DRIVER_FOLDER}"/../profiles
+
+# [TAG] FIRST LOOP FOR COMPILATION!!!
+for d in ${DRIVER_FOLDER}/${DRIVER}.cc
+do
+    echo "Driver: $d"
+    DRIVER_NAME=$(basename "$d")
+    # [TAG] THIS STEP MUST BE ADAPTED FOR EACH LIBRARY
+    # Compile driver for fuzzing
+    $CXX -g -std=c++11  -fsanitize=fuzzer,address -I/"${TARGET}"/work/include \
+        "$d" -Wl,--whole-archive "${TARGET}"/work/lib/libcares.a -Wl,--no-whole-archive \
+        -lz -ljpeg -Wl,-Bstatic -llzma -Wl,-Bdynamic -lstdc++ -o "${d%%.*}"
+
+    # Compile driver for coverage
+    $CXX -g -std=c++11  -fsanitize=fuzzer -fprofile-instr-generate -fcoverage-mapping -I/"${TARGET}"/work/include \
+        "$d" -Wl,--whole-archive  "${TARGET}"/work/lib/libcares_static_profile.a -Wl,--no-whole-archive \
+        -lz -ljpeg -Wl,-Bstatic -llzma -Wl,-Bdynamic -lstdc++ -o "${DRIVER_FOLDER}/../profiles/${DRIVER_NAME%%.*}_profile"
+done
