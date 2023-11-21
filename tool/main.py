1567854adce6e136d20eb739e75bae6179f7d9b8
tregua <flavio87@gmail.com>
2023-11-21 09:17:29 +0000

    fix main missing var

--- tool/main.py
+++ tool/main.py
@@ -29,7 +29,7 @@ def __main():
     # default_config = PROJECT_FOLDER + "/targets/libaom/generator.toml"
     # default_config = PROJECT_FOLDER + "/targets/libpcap/generator.toml"
     # default_config = PROJECT_FOLDER + "/targets/c-ares/generator.toml"
-    # default_config = PROJECT_FOLDER + "/targets/cjson/generator.toml"
+    default_config = PROJECT_FOLDER + "/targets/cjson/generator.toml"
 
     parser = argparse.ArgumentParser(description='Automatic Driver Generator')
     parser.add_argument('--config', type=str, help='The configuration', default=default_config)
