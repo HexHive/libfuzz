b6191b6c1518a826b679d72d3348f69d7c2ad98d
Nicolas Badoux <n.badoux@hotmail.com>
2023-11-29 16:25:40 +0100

    cluster support for libpcap

--- targets/libpcap/compile_driver.sh
+++ targets/libpcap/compile_driver.sh
@@ -18,7 +18,10 @@ cd "$TARGET/repo"
 CXX=$LLVM_DIR/bin/clang++
 
 echo "Compiling: ${DRIVER_FOLDER}/${DRIVER}.cc"
-mkdir -p "${DRIVER_FOLDER}"/../profiles
+PROFILE_DRIVERS="${DRIVER_FOLDER}"/../profiles
+mkdir -p $PROFILE_DRIVERS
+CLUSTER_DRIVERS="${DRIVER_FOLDER}"/../cluster_drivers
+mkdir -p $CLUSTER_DRIVERS
 
 # [TAG] FIRST LOOP FOR COMPILATION!!!
 for d in ${DRIVER_FOLDER}/${DRIVER}.cc
@@ -30,9 +33,14 @@ do
     $CXX -std=c++11 -fsanitize=fuzzer,address -I/"${TARGET}"/work/include \
         "$d" -Wl,--whole-archive "${TARGET}"/work/lib/libpcap.a -Wl,--no-whole-archive \
         -lz -ljpeg -Wl,-Bstatic -llzma -Wl,-Bdynamic -lstdc++ -o "${d%%.*}" || true
+    
+    # Compile driver for clustering
+    $CXX -std=c++11 -fsanitize=fuzzer,address -I/"${TARGET}"/work/include \
+        "$d" -Wl,--whole-archive "${TARGET}"/work/lib/libpcap_cluster.a -Wl,--no-whole-archive \
+        -lz -ljpeg -Wl,-Bstatic -llzma -Wl,-Bdynamic -lstdc++ -o "${CLUSTER_DRIVERS}/${DRIVER_NAME%%.*}_profile" || true
 
     # Compile driver for coverage
     $CXX -g -std=c++11 -fsanitize=fuzzer -fprofile-instr-generate -fcoverage-mapping -I/"${TARGET}"/work/include \
         "$d" -Wl,--whole-archive  "${TARGET}"/work/lib/libpcap_profile.a -Wl,--no-whole-archive \
-        -lz -ljpeg -Wl,-Bstatic -llzma -Wl,-Bdynamic -lstdc++ -o "${DRIVER_FOLDER}/../profiles/${DRIVER_NAME%%.*}_profile" || true
+        -lz -ljpeg -Wl,-Bstatic -llzma -Wl,-Bdynamic -lstdc++ -o "${PROFILE_DRIVERS}/${DRIVER_NAME%%.*}_profile" || true
 done
