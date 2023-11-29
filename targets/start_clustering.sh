6e0be5940e4e54f83f6d914383e927a8a09767ff
tregua <flavio87@gmail.com>
2023-11-29 15:38:02 +0000

    first draft total clustering

--- targets/start_clustering.sh
+++ targets/start_clustering.sh
@@ -17,9 +17,28 @@ FUZZ_TARGETS="$(find ${DRIVER_FOLDER} -type f -executable)"
 
 echo $DRIVER_FOLDER
 
+if [[ $TOTAL_LIBRARY_CLUSTER ]]; then
+
+    CLUSTER_FILTER=misc/cluster_filter.txt
+
+    TARGET_FOLDER=fuzzing_campaigns/total_library_cluster/${TARGET_NAME}
+    TARGET_CRASHES=${TARGET_FOLDER}/crashes
+    TARGET_CLUSTERS=${TARGET_CLUSTER}/clusters
+
+    mkdir -p ${TARGET_CRASHES}
+    mkdir -p ${TARGET_CLUSTERS}
+
+    find ${CRASHES} -name "*.casrep" -exec cp {} ${TARGET_CRASHES} \;
+
+    casr-cluster --ignore ${CLUSTER_FILTER} -c ${TARGET_CRASHES} ${TARGET_CLUSTERS}
+
+    exit 0
+fi
+
 for d in $FUZZ_TARGETS
 do
     DRIVER_NAME=$(basename $d)
+    DRIVER_NAME=${DRIVER_NAME/_cluster/}
     echo "CLUSTERING: ${DRIVER_NAME}"
     casr-libfuzzer -i ${CRASHES}/${DRIVER_NAME} -o ${OUTPUT}/${DRIVER_NAME} -- $d &> ${LOGS}/${DRIVER_NAME}
 done
