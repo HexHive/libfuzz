13fb70bff0c24ed7e78d48c14212e0ad5c1bf207
Nicolas Badoux <n.badoux@hotmail.com>
2023-11-29 16:30:17 +0100

    cluster support for minijail

--- targets/minijail/build_library.sh
+++ targets/minijail/build_library.sh
@@ -41,6 +41,18 @@ make -j$(nproc) OUT="$WORK/lib" CC_STATIC_LIBRARY\(libminijail.pie.a\)
 mv $WORK/lib/libminijail.pie.a $WORK/lib/libminijail_profile.pie.a
 
 
+# Compile library for clustering
+export CFLAGS=$CFLAGS_BASE" -fsanitize=fuzzer-no-link,address -g"
+export CXXFLAGS=$CXXFLAGS_BASE" -fsanitize=fuzzer-no-link,address -g"
+
+echo "make clean"
+make -j$(nproc) clean
+echo "make"
+make -j$(nproc) OUT="$WORK/lib" CC_STATIC_LIBRARY\(libminijail.pie.a\)
+
+mv $WORK/lib/libminijail.pie.a $WORK/lib/libminijail_cluster.pie.a
+
+
 # Compile library for fuzzing
 export CFLAGS=$CFLAGS_BASE" -fsanitize=fuzzer-no-link,address"
 export CXXFLAGS=$CXXFLAGS_BASE" -fsanitize=fuzzer-no-link,address"
--- targets/minijail/compile_driver.sh
+++ targets/minijail/compile_driver.sh
@@ -21,7 +21,10 @@ CXX=$LLVM_DIR/bin/clang++
 CC=$LLVM_DIR/bin/clang
 
 echo "Compiling: ${DRIVER_FOLDER}/${DRIVER}.cc"
-mkdir -p ${DRIVER_FOLDER}/../profiles
+PROFILE_DRIVERS="${DRIVER_FOLDER}"/../profiles
+mkdir -p $PROFILE_DRIVERS
+CLUSTER_DRIVERS="${DRIVER_FOLDER}"/../cluster_drivers
+mkdir -p $CLUSTER_DRIVERS
 
 
 # [TAG] FIRST LOOP FOR COMPILATION!!!
@@ -34,9 +37,14 @@ do
     $CXX -std=c++11  -fsanitize=fuzzer,address -I/${TARGET}/work/include \
         $d -Wl,--whole-archive ${TARGET}/work/lib/libminijail.pie.a -Wl,--no-whole-archive \
         -lcap -Wl,-Bstatic -llzma -Wl,-Bdynamic -lstdc++ -o "${d%%.*}" || true
+    
+    # Compile driver for clustering
+    $CXX -g -std=c++11  -fsanitize=fuzzer,address -I/${TARGET}/work/include \
+        $d -Wl,--whole-archive ${TARGET}/work/lib/libminijail_cluster.pie.a -Wl,--no-whole-archive \
+        -lcap -Wl,-Bstatic -llzma -Wl,-Bdynamic -lstdc++ -o "${CLUSTER_DRIVERS}/${DRIVER_NAME%%.*}_cluster" || true
 
     # Compile driver for coverage
     $CXX -g -std=c++11  -fsanitize=fuzzer -fprofile-instr-generate -fcoverage-mapping \
         -I/${TARGET}/work/include $d -Wl,--whole-archive ${TARGET}/work/lib/libminijail_profile.pie.a -Wl,--no-whole-archive \
-        -lcap -Wl,-Bstatic -llzma -Wl,-Bdynamic -lstdc++ -o "${DRIVER_FOLDER}/../profiles/${DRIVER_NAME%%.*}_profile" || true
+        -lcap -Wl,-Bstatic -llzma -Wl,-Bdynamic -lstdc++ -o "${PROFILE_DRIVERS}/${DRIVER_NAME%%.*}_profile" || true
 done
