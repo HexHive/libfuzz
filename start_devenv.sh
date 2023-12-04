532e90cc6627ad3f83bac7063a016a5f2cec4721
tregua <flavio87@gmail.com>
2023-12-04 08:48:51 +0000

    update start_devenv,sh script

--- start_devenv.sh
+++ start_devenv.sh
@@ -3,5 +3,5 @@
 # docker build -t vsc-libfuzz-230af0437939dbfc00081fc163ef918b -f Dockerfile . 
 # docker run -it -v "$(pwd):/workspaces/libfuzz" vsc-libfuzz-230af0437939dbfc00081fc163ef918b zsh
 
-docker build -t libfuzzpp_dev_image -f Dockerfile . 
-docker run -it -v $(pwd):/home/libfuzz/libfuzz libfuzzpp_dev_image
+DOCKER_BUILDKIT=1 docker build -t libfuzzpp_dev_image -f Dockerfile . 
+docker run -it -v $(pwd):/home/libfuzz/libfuzz libfuzzpp_dev_image zsh
