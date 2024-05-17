47a974412c9232295427ddac5cd3471287b33d34
Flavio Toffalini <flavio.toffalini@epfl.ch>
2024-05-17 12:56:08 +0000

    build llvm fix 1

--- docker/build_llvm.sh
+++ docker/build_llvm.sh
@@ -2,5 +2,6 @@
 
 DOCKER_BUILDKIT=1 docker build --build-arg USER_UID=$(id -u) \
     --build-arg GROUP_UID=$(id -g) --target libfuzzpp_dev_image \
-    -t libfuzzpp_dev_image -f Dockerfile . 
-docker run -v $(pwd):/workspaces/libfuzz libfuzzpp_dev_image /workspaces/libfuzz/bootstrap_llvm.sh
+    -t libfuzzpp_dev_image -f ../Dockerfile ..
+docker run -v $(pwd)/..:/workspaces/libfuzz libfuzzpp_dev_image /workspaces/libfuzz/bootstrap_llvm.sh
+# docker run -it -v $(pwd)/..:/workspaces/libfuzz libfuzzpp_dev_image zsh
