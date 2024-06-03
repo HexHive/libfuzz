1426188df9924a1baf43568df61efa24e6632c84
Flavio Toffalini <flavio.toffalini@epfl.ch>
2024-06-03 08:15:00 +0000

    update commits

--- targets/cpu_features/fetch.sh
+++ targets/cpu_features/fetch.sh
@@ -7,4 +7,4 @@
 
 git clone --no-checkout https://android.googlesource.com/platform/external/cpu_features \
     "$TARGET/repo"
-git -C "$TARGET/repo" checkout 925e059f65798dab125ec08bea999308cf00c86e
+git -C "$TARGET/repo" checkout eca53ba6d2e951e174b64682eaf56a36b8204c89
