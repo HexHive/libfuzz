918f9a5e4f226b387e068fe14d7ad9d9d1eca1a5
Flavio Toffalini <flavio.toffalini@epfl.ch>
2024-07-12 13:33:00 +0000

    improve clean tmp files

--- tool/misc/clean_spurious_files.sh
+++ tool/misc/clean_spurious_files.sh
@@ -3,5 +3,6 @@
 find -maxdepth 1 -type f -regextype posix-basic -regex '^.*/[^/]*[[:cntrl:]][^/]*$' -exec rm {} \;
 find . -maxdepth 1 -name "<fd*" -exec rm {} \;
 rm -f *.bin
+rm -f core.*
 
 echo "[INFO] NOTE: If you have better way to remove temporary files feel free to update $0"
