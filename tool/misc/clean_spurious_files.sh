6ea79fc9a5990cf3ce9bc4aa0395847faf20bb8f
Flavio Toffalini <flavio.toffalini@epfl.ch>
2024-08-25 10:18:51 +0000

    more cleaning

--- tool/misc/clean_spurious_files.sh
+++ tool/misc/clean_spurious_files.sh
@@ -5,4 +5,13 @@ find . -maxdepth 1 -name "<fd*" -exec rm {} \;
 rm -f *.bin
 rm -f core.*
 
+# Find all files and check each for non-printable characters
+find . -maxdepth 1 -type f -print0 | while IFS= read -r -d '' file; do
+        filename=$(basename "$file")
+        if contains_non_printable "$filename"; then
+                # echo "File with non-printable character(s): $file"
+                rm "$file"
+        fi
+done
+
 echo "[INFO] NOTE: If you have better way to remove temporary files feel free to update $0"
