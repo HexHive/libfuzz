bfd949ad25078c0d8053c228f89000dc4b56a395
Nicolas Badoux <nicolas.badoux@epfl.ch>
2025-01-24 13:54:24 +0000

    clean spurious file

--- tool/misc/clean_spurious_files.sh
+++ tool/misc/clean_spurious_files.sh
@@ -4,14 +4,7 @@ find -maxdepth 1 -type f -regextype posix-basic -regex '^.*/[^/]*[[:cntrl:]][^/]
 find . -maxdepth 1 -name "<fd*" -exec rm {} \;
 rm -f *.bin
 rm -f core.*
-
-# Find all files and check each for non-printable characters
-find . -maxdepth 1 -type f -print0 | while IFS= read -r -d '' file; do
-        filename=$(basename "$file")
-        if contains_non_printable "$filename"; then
-                # echo "File with non-printable character(s): $file"
-                rm "$file"
-        fi
-done
+# Remove files that do not have alphanumeric characters, dots, underscores, or hyphens
+find . -maxdepth 1 -type f ! -name '[a-zA-Z0-9._\- ()]*' -exec rm {} \;
 
 echo "[INFO] NOTE: If you have better way to remove temporary files feel free to update $0"
