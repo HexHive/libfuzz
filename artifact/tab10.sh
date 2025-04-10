a9c6b4e06c0db7a3bf9e56d4452a0bb53ee6ccd7
Flavio Toffalini <flavio87@gmail.com>
2025-04-10 09:56:46 +0000

    script for table 10

---
+++ artifact/tab10.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ../fuzzing_campaigns
+
+export CONF=grammar
+source campaign_configuration.sh
+
+GEN24_DEEP0=./gen24_deep0
+
+./select_stable_drivers_cluster.py -d ${GEN24_DEEP0} -t ${TIMEOUT} -simulate table 2> /dev/null
+
+rm -f config.txt
